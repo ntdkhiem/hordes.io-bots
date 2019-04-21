1ab7344e39aed770062c73b44a197f8c923c887a
TopKeingt <ppkhiemnguyen@gmail.com>
2019-04-20 23:39:32 -0400

    - More precise duration between each skill

--- core/characters/mage.py
+++ core/characters/mage.py
@@ -9,8 +9,8 @@ class Mage(Bot):
             "1": {
                 "key": 2,
                 "name": "Frostcall",
-                "Cooldown": 7,
-                "Duration": 4,
+                "Cooldown": 3,
+                "Duration": 5,
                 "MPCost": 22.4
             },
             "2": {
@@ -26,7 +26,7 @@ class Mage(Bot):
                 "key": 4,
                 "name": "Iceblock",
                 "Cooldown": 18,
-                "Duration": 4,
+                "Duration": 5,
                 "MPCost": 33.6
             },
             "2": {
@@ -50,30 +50,38 @@ class Mage(Bot):
         
     
     def attack(self):
-        if time.time() - self.attack_countdown > self.attack_1.get("Cooldown"):
-            # self.attack_1     Run
-            self.action.send_keys(self.attack_1.get("key"))
-            self.attack_countdown = time.time()
-            logger.debug("Wait for attacking skill to finish...")
-            time.sleep(self.attack_1.get("Duration"))                   # Wait for the skill's to finish
-        else:
-            # self.attack_2     Run
-            self.action.send_keys(self.attack_2.get("key"))
-            logger.debug("Wait for attacking skill to finish...")
-            time.sleep(self.attack_2.get("Duration"))                   # Wait for the skill's to finish
+        try:
+                
+            if time.time() - self.attack_countdown > self.attack_1.get("Cooldown"):
+                # self.attack_1     Run
+                self.action.send_keys(self.attack_1.get("key"))
+                self.attack_countdown = time.time()
+                logger.debug("Wait for attacking skill to finish...")
+                time.sleep(self.attack_1.get("Duration"))                   # Wait for the skill's to finish
+            else:
+                # self.attack_2     Run
+                self.action.send_keys(self.attack_2.get("key"))
+                logger.debug("Wait for attacking skill to finish...")
+                time.sleep(self.attack_2.get("Duration"))                   # Wait for the skill's to finish
+        except KeyboardInterrupt:
+            self.quit()
             
     
     def defend(self):
-        if time.time() - self.defend_1_countdown > self.defend_1.get("Cooldown"):
-            # self.defend_1     Run
-            self.action.send_keys(self.defend_1.get("key"))
-            self.defend_1_countdown = time.time()
-            logger.debug("Wait for defending skill to finish...")
-            time.sleep(self.defend_1.get("Duration"))                   # Wait for the skill's to finish
-        else:
-            # self.defend_2     Run
-            if time.time() - self.defend_2_countdown > self.defend_2.get("Cooldown"):
-                self.action.send_keys(self.defend_2.get("key"))
-                self.defend_2_countdown = time.time()
+        try:
+
+            if time.time() - self.defend_1_countdown > self.defend_1.get("Cooldown"):
+                # self.defend_1     Run
+                self.action.send_keys(self.defend_1.get("key"))
+                self.defend_1_countdown = time.time()
                 logger.debug("Wait for defending skill to finish...")
-                time.sleep(self.defend_2.get("Duration"))                   # Wait for the skill's to finish
+                time.sleep(self.defend_1.get("Duration"))                   # Wait for the skill's to finish
+            else:
+                # self.defend_2     Run
+                if time.time() - self.defend_2_countdown > self.defend_2.get("Cooldown"):
+                    self.action.send_keys(self.defend_2.get("key"))
+                    self.defend_2_countdown = time.time()
+                    logger.debug("Wait for defending skill to finish...")
+                    time.sleep(self.defend_2.get("Duration"))                   # Wait for the skill's to finish
+        except KeyboardInterrupt:
+            self.quit()
